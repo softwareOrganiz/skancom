a6be2531a2e898ef4d3e2bd2d07fc7e6ac7f32fc
mona <mona@DESKTOP-3FD312V>
2023-08-03 17:19:11 +0300

    note

---
+++ src/test/java/acceptance/adminacceptance.java
@@ -0,0 +1,53 @@
+package acceptance;
+import io.cucumber.java.en.*;
+
+public class adminacceptance {
+	@Given("want to show waiting requests")
+	public void want_to_show_waiting_requests() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("enter a valid id house")
+	public void enter_a_valid_id_house() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("he press {int}")
+	public void he_press(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the house request is published to tenants")
+	public void the_house_request_is_published_to_tenants() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("delete it from the waiting list")
+	public void delete_it_from_the_waiting_list() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the house request is deleted from the waiting list")
+	public void the_house_request_is_deleted_from_the_waiting_list() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("enter  unvalid id house")
+	public void enter_unvalid_id_house() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("give him a message that it is a false house id")
+	public void give_him_a_message_that_it_is_a_false_house_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
